2ba26cba292514ac94dbd92a34cb95df32d64acd
maxi boch <private@example.com>
2025-11-27 02:27:34 -0500

    Add release automation and FMOD layout cleanup utility

--- Editor/GraphyFmodOverlayBuilder.cs
+++ Editor/GraphyFmodOverlayBuilder.cs
@@ -37,9 +37,8 @@ namespace Tayx.Graphy.FmodEditor
             vertical.childAlignment = TextAnchor.UpperLeft;
             vertical.spacing = 4f;
             vertical.padding = new RectOffset(4, 4, 4, 4);
-
-            var fitter = rootGo.AddComponent<ContentSizeFitter>();
-            fitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            vertical.childForceExpandHeight = false;
+            vertical.childControlHeight = true;
 
             // Graphy manager
             var graphyManagerGo = new GameObject("GraphyManager", typeof(RectTransform));
@@ -48,7 +47,7 @@ namespace Tayx.Graphy.FmodEditor
             var graphyManager = graphyManagerGo.AddComponent<GraphyManager>();
 
 #if GRAPHY_FMOD || UNITY_EDITOR
-            var fmodManagerGo = new GameObject("FMOD Module", typeof(RectTransform));
+            var fmodManagerGo = new GameObject("FMOD - Module", typeof(RectTransform));
             Undo.RegisterCreatedObjectUndo(fmodManagerGo, "Create FMOD Module");
             fmodManagerGo.transform.SetParent(rootGo.transform, false);
 
@@ -56,57 +55,79 @@ namespace Tayx.Graphy.FmodEditor
             var fmodMonitor = fmodManagerGo.AddComponent<G_FmodMonitor>();
             var fmodText = fmodManagerGo.AddComponent<G_FmodText>();
             var fmodGraph = fmodManagerGo.AddComponent<G_FmodGraph>();
-
-            // Simple stacked layout for FMOD section so user gets an immediate clean view.
-            var fmodLayout = fmodManagerGo.AddComponent<VerticalLayoutGroup>();
-            fmodLayout.childAlignment = TextAnchor.UpperLeft;
-            fmodLayout.spacing = 2f;
-            fmodLayout.padding = new RectOffset(4, 4, 4, 4);
-
-            // Background
-            var bgImage = fmodManagerGo.AddComponent<Image>();
-            bgImage.color = new Color(0f, 0f, 0f, 0.5f);
-
-            // Title
-            var title = CreateText("FMOD", fmodManagerGo.transform, 12, FontStyle.Bold, TextAnchor.MiddleLeft);
-            // Current lines
-            var cpuText = CreateText("FMOD CPU:", fmodManagerGo.transform, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
-            var memText = CreateText("FMOD Mem:", fmodManagerGo.transform, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
-            var chText = CreateText("Channels:", fmodManagerGo.transform, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
-            var fileText = CreateText("File I/O:", fmodManagerGo.transform, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
-
-            // Avg / Peak rows
-            var cpuAvg = CreateText("Avg:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-            var memAvg = CreateText("Avg:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-            var chAvg = CreateText("Avg:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-            var fileAvg = CreateText("Avg:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-
-            var cpuPeak = CreateText("Peak:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-            var memPeak = CreateText("Peak:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-            var chPeak = CreateText("Peak:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-            var filePeak = CreateText("Peak:", fmodManagerGo.transform, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
-
-            // Wire up FMOD text component
-            fmodText.GetType().GetField("m_fmodCpuText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, cpuText);
-            fmodText.GetType().GetField("m_fmodMemoryText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, memText);
-            fmodText.GetType().GetField("m_channelsText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, chText);
-            fmodText.GetType().GetField("m_fileUsageText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, fileText);
-
-            fmodText.GetType().GetField("m_fmodCpuAvgText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, cpuAvg);
-            fmodText.GetType().GetField("m_fmodMemoryAvgText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, memAvg);
-            fmodText.GetType().GetField("m_channelsAvgText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, chAvg);
-            fmodText.GetType().GetField("m_fileUsageAvgText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, fileAvg);
-
-            fmodText.GetType().GetField("m_fmodCpuPeakText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, cpuPeak);
-            fmodText.GetType().GetField("m_fmodMemoryPeakText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, memPeak);
-            fmodText.GetType().GetField("m_channelsPeakText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, chPeak);
-            fmodText.GetType().GetField("m_fileUsagePeakText", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.SetValue(fmodText, filePeak);
+            BuildFmodSection(fmodManagerGo.transform, fmodText);
 
             // Make sure GraphyManager can find the FMOD manager/monitor
             graphyManagerGo.transform.SetParent(rootGo.transform, false);
 #endif
 
             Selection.activeGameObject = rootGo;
+        }
+
+        [MenuItem("Tools/Graphy/Clean Selected Graphy FMOD Layout", false, 1010)]
+        private static void CleanSelectedFmodLayout()
+        {
+            var selected = Selection.activeGameObject;
+            if (selected == null)
+            {
+                Debug.LogWarning("[Graphy] No GameObject selected. Select your [Graphy] instance or a child of it first.");
+                return;
+            }
+
+            var graphyManager = selected.GetComponentInParent<GraphyManager>();
+            if (graphyManager == null)
+            {
+                graphyManager = selected.GetComponentInChildren<GraphyManager>();
+            }
+
+            if (graphyManager == null)
+            {
+                Debug.LogWarning("[Graphy] Selected object is not part of a Graphy hierarchy.");
+                return;
+            }
+
+#if GRAPHY_FMOD || UNITY_EDITOR
+            var fmodManager = graphyManager.GetComponentInChildren<G_FmodManager>(true);
+            if (fmodManager == null)
+            {
+                Debug.LogWarning("[Graphy] Could not find G_FmodManager under the selected Graphy instance.");
+                return;
+            }
+
+            var fmodManagerGo = fmodManager.gameObject;
+            Undo.RegisterFullObjectHierarchyUndo(fmodManagerGo, "Clean Graphy FMOD Layout");
+
+            // Prefer an explicit FMOD_Text container if present so we don't touch graphs/spectrum/audio levels.
+            Transform fmodTextRoot = fmodManagerGo.transform.Find("FMOD_Text");
+
+            var fmodText = fmodManagerGo.GetComponentInChildren<G_FmodText>(true);
+            if (fmodText == null)
+            {
+                if (fmodTextRoot == null)
+                {
+                    fmodTextRoot = new GameObject("FMOD_Text", typeof(RectTransform)).transform;
+                    fmodTextRoot.SetParent(fmodManagerGo.transform, false);
+                }
+
+                fmodText = Undo.AddComponent<G_FmodText>(fmodTextRoot.gameObject);
+            }
+            else if (fmodTextRoot == null)
+            {
+                fmodTextRoot = fmodText.transform;
+            }
+
+            // Clear only the text container's children, leave FMOD_Graph, Spectrum_Graph, AudioLevels_Container intact.
+            for (int i = fmodTextRoot.childCount - 1; i >= 0; i--)
+            {
+                var child = fmodTextRoot.GetChild(i);
+                Undo.DestroyObjectImmediate(child.gameObject);
+            }
+
+            BuildFmodSection(fmodTextRoot, fmodText);
+            Selection.activeGameObject = fmodTextRoot.gameObject;
+#else
+            Debug.LogWarning("[Graphy] FMOD support is not enabled in this build.");
+#endif
         }
 
         private static Canvas GetOrCreateCanvas()
@@ -127,6 +148,69 @@ namespace Tayx.Graphy.FmodEditor
             scaler.referenceResolution = new Vector2(1920, 1080);
 
             return canvas;
+        }
+
+        private static void BuildFmodSection(Transform parent, G_FmodText fmodText)
+        {
+            // Simple stacked layout for FMOD section so user gets an immediate clean view.
+            var layoutHost = parent.gameObject;
+
+            var existingLayout = layoutHost.GetComponent<VerticalLayoutGroup>();
+            if (existingLayout == null)
+            {
+                existingLayout = layoutHost.AddComponent<VerticalLayoutGroup>();
+            }
+
+            var fmodLayout = existingLayout;
+            fmodLayout.childAlignment = TextAnchor.UpperLeft;
+            fmodLayout.spacing = 2f;
+            fmodLayout.padding = new RectOffset(4, 4, 4, 4);
+            fmodLayout.childForceExpandHeight = false;
+            fmodLayout.childControlHeight = true;
+
+            // Background
+            var bgImage = layoutHost.GetComponent<Image>();
+            if (bgImage == null)
+            {
+                bgImage = layoutHost.AddComponent<Image>();
+            }
+            bgImage.color = new Color(0f, 0f, 0f, 0.5f);
+
+            // Title
+            var title = CreateText("FMOD", parent, 12, FontStyle.Bold, TextAnchor.MiddleLeft);
+            // Current lines
+            var cpuText = CreateText("FMOD CPU:", parent, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
+            var memText = CreateText("FMOD Mem:", parent, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
+            var chText = CreateText("Channels:", parent, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
+            var fileText = CreateText("File I/O:", parent, 11, FontStyle.Normal, TextAnchor.MiddleLeft);
+
+            // Avg / Peak rows
+            var cpuAvg = CreateText("Avg:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+            var memAvg = CreateText("Avg:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+            var chAvg = CreateText("Avg:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+            var fileAvg = CreateText("Avg:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+
+            var cpuPeak = CreateText("Peak:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+            var memPeak = CreateText("Peak:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+            var chPeak = CreateText("Peak:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+            var filePeak = CreateText("Peak:", parent, 10, FontStyle.Italic, TextAnchor.MiddleLeft);
+
+            // Wire up FMOD text component
+            var flags = System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance;
+            fmodText.GetType().GetField("m_fmodCpuText", flags)?.SetValue(fmodText, cpuText);
+            fmodText.GetType().GetField("m_fmodMemoryText", flags)?.SetValue(fmodText, memText);
+            fmodText.GetType().GetField("m_channelsText", flags)?.SetValue(fmodText, chText);
+            fmodText.GetType().GetField("m_fileUsageText", flags)?.SetValue(fmodText, fileText);
+
+            fmodText.GetType().GetField("m_fmodCpuAvgText", flags)?.SetValue(fmodText, cpuAvg);
+            fmodText.GetType().GetField("m_fmodMemoryAvgText", flags)?.SetValue(fmodText, memAvg);
+            fmodText.GetType().GetField("m_channelsAvgText", flags)?.SetValue(fmodText, chAvg);
+            fmodText.GetType().GetField("m_fileUsageAvgText", flags)?.SetValue(fmodText, fileAvg);
+
+            fmodText.GetType().GetField("m_fmodCpuPeakText", flags)?.SetValue(fmodText, cpuPeak);
+            fmodText.GetType().GetField("m_fmodMemoryPeakText", flags)?.SetValue(fmodText, memPeak);
+            fmodText.GetType().GetField("m_channelsPeakText", flags)?.SetValue(fmodText, chPeak);
+            fmodText.GetType().GetField("m_fileUsagePeakText", flags)?.SetValue(fmodText, filePeak);
         }
 
         private static Text CreateText(string initialText, Transform parent, int fontSize, FontStyle style, TextAnchor alignment)
---
+++ RELEASE_GUIDE.md
@@ -0,0 +1,70 @@
+# Release Automation Guide
+
+This repository includes two ways to automate the release process.
+
+## Quick Release Commands
+
+### Option 1: PowerShell Script (Recommended for local use)
+
+```powershell
+# Simple release with just version and message
+.\release.ps1 -Version "3.1.13" -Message "Fix bug in FMOD integration"
+
+# Release with custom release notes
+.\release.ps1 -Version "3.1.13" -Message "Major update" -ReleaseNotes "## Features`n- New feature 1`n- New feature 2"
+```
+
+**What it does:**
+1. Commits any uncommitted changes
+2. Pushes to GitHub
+3. Updates package.json version
+4. Creates and pushes version tag (e.g., v3.1.13)
+5. Updates the 'latest' tag
+6. Creates GitHub release with notes
+
+### Option 2: GitHub Actions Workflow
+
+1. Go to your repository on GitHub
+2. Click on "Actions" tab
+3. Select "Create Release" workflow
+4. Click "Run workflow"
+5. Fill in:
+   - Version number (e.g., 3.1.13)
+   - Release message/title
+   - Release notes (optional)
+6. Click "Run workflow"
+
+**What it does:**
+Same as the PowerShell script, but runs on GitHub's servers.
+
+## Manual Process (if needed)
+
+If you prefer to do it manually or need more control:
+
+```powershell
+# 1. Commit your changes
+git add .
+git commit -m "Your commit message"
+git push origin main
+
+# 2. Update package.json version (edit manually or via script)
+
+# 3. Create and push tag
+git tag -a v3.1.13 -m "v3.1.13: Your message"
+git push origin v3.1.13
+
+# 4. Update latest tag
+git tag -f latest v3.1.13
+git push origin latest --force
+
+# 5. Create GitHub release
+gh release create v3.1.13 --title "v3.1.13: Your title" --notes "Your release notes"
+```
+
+## Best Practices
+
+1. Always ensure your code is tested before releasing
+2. Use semantic versioning (MAJOR.MINOR.PATCH)
+3. Write clear, descriptive release messages
+4. Include breaking changes in release notes
+5. Tag Unity-specific releases appropriately
